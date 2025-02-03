a29364dcf6b290abe6b51c5294903e8aa0d748d5
rchebbi <rchebbi@GCFRPA043>
2025-02-03 12:42:56 +0100

    game . cpp et . h

--- Zelda Like Groupe 3/Game.cpp
+++ Zelda Like Groupe 3/Game.cpp
@@ -1,9 +1,32 @@
 #include "Game.h"
-
+#include <SFML/Graphics.hpp>
 Game::Game() {
 	//enemy1.push_back(std::make_unique<Enemy1>(Vector2f(4400.0f, 2200.f)));
 	//for (auto it = enemy1.begin(); it != enemy1.end();) {
 		//std::cout << "ok";
 	//}
+
+	Game::Game() {}
+	void Game::win() {
+		sf::CircleShape shape(100.f);
+		shape.setFillColor(sf::Color::Black);
+		if (GAME_STATE == WIN) {
+			void draw(sf::RenderWindow & window)
+				setText("You Win !")
+		}
+	}
+	void Game::lose() {
+		if (GAME_STATE == Loose) {
+			void draw(sf::RenderWindow & window)
+				setText("Too Bad , You lost ;-;")
+		}
+	}
+	void draw(sf::RenderWindow & window) {
+		window.clear();
+		window.draw(shape);
+	}
+
+
+}
 };
 
--- Zelda Like Groupe 3/Game.h
+++ Zelda Like Groupe 3/Game.h
@@ -4,11 +4,18 @@
 #include "Player.h"
 #include "Enemy1.h"
 
-
+enum GAME_STATE { MENU, PLAYING, WIN, LOSE };
 class Game {
 private:
 public:
-	//vector<unique_ptr<Enemy1>> enemy1;
-	PropManager propManager;
-	Game();
+	int x;
+	int y;
+	Game(int x, int y);
+	
+	sf::RectangleShape shape(x ,y );
+	GAME_STATE state = PLAYING;
+	void win();
+	void lose();
+	void draw(sf::RenderWindow& window);
+	};
 };
