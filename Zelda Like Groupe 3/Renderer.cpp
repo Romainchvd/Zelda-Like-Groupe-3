0c2399449056bcc5c6ea49ba12e8e2b704323ac0
Romain Dumont <rominou.dumont@gmail.com>
2025-02-06 05:12:35 +0100

    Correction code mal fusionné

--- Zelda Like Groupe 3/Renderer.cpp
+++ Zelda Like Groupe 3/Renderer.cpp
@@ -63,80 +63,95 @@ void Renderer::run(Player& player, PropManager& propManager, vector<unique_ptr<E
 	View camera(View(Vector2f(100, 100), Vector2f(1920.f, 1080.f)));
 	camera.setSize(Vector2f(1920.f / 2, 1080.f / 2));
 	while (window.isOpen()) {
-		player.swordAttackCheck();
-		if (player.isAttacking) {
-			player.swordAttack(enemy1[0]);
-			player.swordAttack(garde[0]);
-		}
-		boss.Movement(player);
-		boss.update(1);
-		player.Mouvement();
-		player.update(1);
-		for (auto& enemy : enemy1) {
-			enemy->update(1);
-			enemy->updateMovement(player,0.05f);
-		}
-		for (auto& garde1 : garde) {
-			garde1->update(1);
-			garde1->updateMovement(player, 0.05f);
-		}
-		Draw(player, propManager, enemy1, garde, camera, boss);
-		camera.setCenter(player.sprite.getPosition());
-		window.setView(camera);
-		for (int i = 0; i < propManager.getSecondLayer().size(); i++)
+		if (game.state == PLAYING)
 		{
-			propManager.getSecondLayer()[i]->collectProp(player, propManager);
-			propManager.getSecondLayer()[i]->useKey(player, propManager);
-		}
-		//colision player
-		for (int i = 0; i < propManager.getFirstLayer().size(); i++) {
-			player.Colision(propManager.getFirstLayer()[i]);
-			player.Interact(propManager.getFirstLayer()[i], window);
+			player.swordAttackCheck();
+			if (player.isAttacking) {
+				player.swordAttack(enemy1[0]);
+				player.swordAttack(garde[0]);
+			}
+			boss.Movement(player);
+			boss.update(1);
+			player.Mouvement();
+			player.update(1);
 			for (auto& enemy : enemy1) {
-				enemy->Colision(propManager.getFirstLayer()[i]);
+				enemy->update(1);
+				enemy->updateMovement(player, 0.05f);
 			}
 			for (auto& garde1 : garde) {
-				garde1->Colision(propManager.getFirstLayer()[i]);
+				garde1->update(1);
+				garde1->updateMovement(player, 0.05f);
 			}
-		}
-		for (int i = 0; i < propManager.getSecondLayer().size(); i++) {
-			player.Colision(propManager.getSecondLayer()[i]);
-			player.Interact(propManager.getSecondLayer()[i], window);
-			for (auto& enemy : enemy1) {
-				enemy->Colision(propManager.getSecondLayer()[i]);
-			}
-			for (auto& garde1 : garde) {
-				garde1->Colision(propManager.getSecondLayer()[i]);
-			}
-		}
-		//
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
+			Draw(player, propManager, enemy1, garde, camera, boss);
+			camera.setCenter(player.sprite.getPosition());
+			window.setView(camera);
+			for (int i = 0; i < propManager.getSecondLayer().size(); i++)
 			{
-				window.close();
-				running = false;
+				propManager.getSecondLayer()[i]->collectProp(player, propManager);
+				propManager.getSecondLayer()[i]->useKey(player, propManager);
 			}
-			if (event.type == Event::KeyPressed)
+			//colision player
+			for (int i = 0; i < propManager.getFirstLayer().size(); i++) {
+				player.Colision(propManager.getFirstLayer()[i]);
+				player.Interact(propManager.getFirstLayer()[i], window);
+				for (auto& enemy : enemy1) {
+					enemy->Colision(propManager.getFirstLayer()[i]);
+				}
+				for (auto& garde1 : garde) {
+					garde1->Colision(propManager.getFirstLayer()[i]);
+				}
+			}
+			for (int i = 0; i < propManager.getSecondLayer().size(); i++) {
+				player.Colision(propManager.getSecondLayer()[i]);
+				player.Interact(propManager.getSecondLayer()[i], window);
+				for (auto& enemy : enemy1) {
+					enemy->Colision(propManager.getSecondLayer()[i]);
+				}
+				for (auto& garde1 : garde) {
+					garde1->Colision(propManager.getSecondLayer()[i]);
+				}
+			}
+			//
+			while (window.pollEvent(event))
 			{
-				if (event.key.code == Keyboard::Enter)
+				if (event.type == Event::Closed)
 				{
 					window.close();
-					running = false;
+				}
+				if (event.type == Event::KeyPressed)
+				{
+					if (event.key.code == Keyboard::Enter)
+					{
+						running = false;
+						if (musicThread.joinable()) {
+							musicThread.join();
+						}
+						window.close();
+					}
 				}
 			}
 		}
-	}
-	musicThread.join();
 		if (game.state == MENU)
 		{
 			while (window.pollEvent(event))
 			{
 				game.mousePosition = Mouse::getPosition(window);
 				if (event.type == Event::Closed)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Enter)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 				if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left)
 				{
 					if (game.playS.getGlobalBounds().contains(static_cast<Vector2f>(game.mousePosition)))
@@ -163,9 +178,21 @@ void Renderer::run(Player& player, PropManager& propManager, vector<unique_ptr<E
 			while (window.pollEvent(event))
 			{
 				if (event.type == Event::Closed)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Enter)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 			}
 			window.clear();
 			window.draw(game.lostS);
@@ -178,68 +205,25 @@ void Renderer::run(Player& player, PropManager& propManager, vector<unique_ptr<E
 			while (window.pollEvent(event))
 			{
 				if (event.type == Event::Closed)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Enter)
+				{
+					running = false;
+					if (musicThread.joinable()) {
+						musicThread.join();
+					}
 					window.close();
+				}
 			}
 			window.clear();
 			window.draw(game.wonS);
 			window.display();
 		}
-		if (game.state == PLAYING)
-		{
-			player.swordAttackCheck();
-			if (player.isAttacking)
-				player.swordAttack(enemy1[0]);
-			player.Mouvement();
-			player.update(1);
-			boss.update(1);
-			boss.Movement(player);
-			for (auto& enemy : enemy1) {
-				enemy->update(1);
-				enemy->updateMovement(player, 0.05f);
-			}
-			Draw(player, propManager, enemy1, garde, camera, boss);
-			camera.setCenter(player.sprite.getPosition());
-			window.setView(camera);
-			for (int i = 0; i < propManager.getSecondLayer().size(); i++)
-			{
-				propManager.getSecondLayer()[i]->collectProp(player, propManager);
-				propManager.getSecondLayer()[i]->useKey(player, propManager);
-			}
-			//colision player
-			for (int i = 0; i < propManager.getFirstLayer().size(); i++) {
-				player.Colision(propManager.getFirstLayer()[i]);
-				player.Interact(propManager.getFirstLayer()[i], window);
-				for (auto& enemy : enemy1) {
-					enemy->Colision(propManager.getFirstLayer()[i]);
-				}
-			}
-			for (int i = 0; i < propManager.getSecondLayer().size(); i++) {
-				player.Colision(propManager.getSecondLayer()[i]);
-				player.Interact(propManager.getSecondLayer()[i], window);
-				for (auto& enemy : enemy1) {
-					enemy->Colision(propManager.getSecondLayer()[i]);
-				}
-			}
-			while (window.pollEvent(event))
-			{
-				if (event.type == Event::Closed)
-				{
-					window.close();
-					running = false;
-				}
-				if (event.type == Event::KeyPressed)
-				{
-					if (event.key.code == Keyboard::Enter)
-					{
-						window.close();
-						running = false;
-					}
-				}
-			}
-		}
-	running = false;
-	if (musicThread.joinable()) 
-		musicThread.join();
+	}
 }
